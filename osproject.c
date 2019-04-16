c822471ef4b207fe9caa5ff26c4070cdffaefc66
ashutosh00007 <49312384+ashutosh00007@users.noreply.github.com>
2019-04-16 19:02:58 +0530

    wrote till line 18th

---
+++ osproject.c
@@ -0,0 +1,19 @@
+#include<stdio.h>
+#include<conio.h>
+#include<string.h>
+void main()
+{
+	char p[10][5],temp[5];
+	int i,j,pt[10],wt[10],totwt=0,pr[10],temp1,n;
+	float avgwt;
+	printf("Enter no of girls:");
+	scanf("%d",&n);
+	for(i=0;i<n;i++)
+	{
+		printf("enter girl %d name:",i+1);
+  		scanf("%s",&p[i]);
+		printf("enter process time:");
+		scanf("%d",&pt[i]);
+		printf("enter no of gifts:");
+		scanf("%d",&pr[i]);
+	}
