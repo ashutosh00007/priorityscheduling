9cf0823b72e22b07064fa3ae43db8f13dd546a34
ashutosh00007 <49312384+ashutosh00007@users.noreply.github.com>
2019-04-17 19:18:55 +0530

    sorting Algo is inserted from line 20 to line 37

--- osproject.c
+++ osproject.c
@@ -17,3 +17,21 @@ void main()
 		printf("enter no of gifts:");
 		scanf("%d",&pr[i]);
 	}
+	for(i=0;i<n-1;i++)
+	{
+		for(j=i+1;j<n;j++)
+		{
+			if(pr[i]>pr[j])
+			{
+				temp1=pr[i];
+				pr[i]=pr[j];
+				pr[j]=temp1;
+				temp1=pt[i];
+				pt[i]=pt[j];
+				pt[j]=temp1;
+				strcpy(temp,p[i]);
+				strcpy(p[i],p[j]);
+				strcpy(p[j],temp);
+			}
+		}
+	}
