8c7008aed4569c57f388ac7f52635da1d2150d79
ashutosh00007 <49312384+ashutosh00007@users.noreply.github.com>
2019-04-19 02:20:47 +0530

    now fully completed

--- osproject.c
+++ osproject.c
@@ -24,3 +24,52 @@ void getdata()
     }
     
 }
+
+void showValues()
+{
+    
+    System.out.println((j+1)+"       "+name+"               "+sum);
+    j++;
+	
+    
+}
+
+    
+    public static void main(String[] args) 
+    {
+       int n;
+       Scanner sc=new Scanner(System.in);
+       System.out.println("Enter the no. of students");
+       n=sc.nextInt();
+        Giftshop temp=new Giftshop();
+        Giftshop [] a=new Giftshop[n];
+        for(int i=0;i<n;i++)
+        {
+            a[i]=new Giftshop();
+            a[i].getdata();
+        }
+  
+ 
+        for(int i=0; i<n;i++) 
+        {
+         for(int j=1;j <(n-i);j++) 
+         {
+            if(a[j-1].sum<a[j].sum) 
+            {
+               temp = a[j-1];
+               a[j-1] = a[j];
+               a[j] = temp;
+            }
+         }
+        }
+        
+        System.out.println("Sno.    Student_name    total_gifts");
+        
+        for(int i=0;i<n;i++)
+        {
+            a[i].showValues();
+        }
+    }
+     
+    
+}
